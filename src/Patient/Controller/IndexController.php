1c951d91c323e90228a3bed83458cd841746e827
Dzmitry Lukashuk <dmitry.lukashuk@sobrado.ch>
2023-05-14 20:59:13 +0200

    Added Controller and view

---
+++ src/Patient/Controller/IndexController.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Patient\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\Cache;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Controller used to manage blog contents in the public part of the site.
+ *
+ * @author Ryan Weaver <weaverryan@gmail.com>
+ * @author Javier Eguiluz <javier.eguiluz@gmail.com>
+ */
+#[Route('/medical-card')]
+final class IndexController extends AbstractController
+{
+    /**
+     * NOTE: For standard formats, Symfony will also automatically choose the best
+     * Content-Type header for the response.
+     *
+     * See https://symfony.com/doc/current/routing.html#special-parameters
+     */
+    #[Route('/{userId}', name: 'card_index', defaults: ['userId' => '1', '_format' => 'html'], methods: ['GET'])]
+    #[Cache(smaxage: 10)]
+    public function index(Request $request, int $userId, string $_format): Response
+    {
+        return $this->render('medical-card/index.'.$_format.'.twig', [
+            'userId' => $userId,
+        ]);
+    }
+}
